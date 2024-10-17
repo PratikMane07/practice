9cc6ad77d2f48cd6e387e7ccb1515f6c12bb8a05
omkar3720 <omkarshinde2728@gmail.com>
2024-10-17 09:02:38 +0530

    add Multiplication Method

--- calculator/src/main/java/com/app/controller/HomeController.java
+++ calculator/src/main/java/com/app/controller/HomeController.java
@@ -14,7 +14,13 @@ public class HomeController {
 	         int result = num1 + num2;	
 	         return result;
 	}
-	
-	
-	
+	@GetMapping("/mul/{num1}/{num2}")
+	public int multiplicationByOmkar(@PathVariable int num1, @PathVariable int num2)
+	{
+
+        int result = num1 * num2;	
+        
+        return result;
+	}
+		
 }
