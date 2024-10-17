4fbcd2726c974c92e5f35d0920b15b129503189c
yogesh5135 <yogesh5135@gmail.com>
2024-10-17 11:07:07 +0530

    division by Yogesh

--- calculator/src/main/java/com/app/controller/HomeController.java
+++ calculator/src/main/java/com/app/controller/HomeController.java
@@ -23,4 +23,10 @@ public class HomeController {
         return result;
 	}
 		
+	@GetMapping("/div/{num1}/{num2}")
+	public int division(@PathVariable int num1, @PathVariable int num2)
+	{
+	         int result = num1 / num2;	
+	         return result;
+	}
 }
